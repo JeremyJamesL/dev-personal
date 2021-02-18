e4dbfc3e9b40e6249ad68605c46a8f6ceeadde39
Jeremy Luscombe <jeremyluscombe@gmail.com>
2021-02-18 10:16:02 +0000

    intial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Jeremy James Digtial Marketing Portfolio
+
+Simple one page portfolio with work history, CV and example work. 
---
+++ script.js
@@ -0,0 +1,20 @@
+// Open Nav
+
+function openNav() {
+    document.querySelector('.main-menu').classList.toggle('main-menu--open');
+    document.querySelector('.menu-toggle').classList.toggle('menu-toggle--black');
+}
+
+
+// Color Picker
+
+function colorPicker() {
+    let color = this.getAttribute('data-color');
+    document.body.className = `theme-${color}`;
+}
+
+document.querySelectorAll('.theme-picker__choice').forEach( el => {
+    el.addEventListener("click", colorPicker);
+});
+
+
